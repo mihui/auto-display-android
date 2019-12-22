27da9a0e4759e26e9d006337f07179e52cafb6d9
Michael <mihui@cn.ibm.com>
2019-12-23 07:55:29 +0800

    Working In Progress

---
+++ app/src/main/kotlin/com/ibm/cic/kotlin/starterkit/helpers/MatrixHelper.kt
@@ -0,0 +1,123 @@
+package com.ibm.cic.kotlin.starterkit.helpers
+
+import android.content.Context
+import java.io.InputStream
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MatrixHelper(private val context: Context) {
+
+    private val all_16_32 = 16
+    private val all_2_4 = 2
+    private val all_32_128 = 32
+
+    private fun toByteArray(x: Char, y: Char): ByteArray {
+
+        return byteArrayOf('0'.toByte(), 'x'.toByte(), x.toUpperCase().toByte(), y.toUpperCase().toByte())
+    }
+
+    fun getMatrixList(str: String, index: Int): ByteArray {
+
+        var data: ByteArray? = null
+        var code: IntArray? = null
+        var byteCount: Int
+
+        for (i in str.indices) {
+
+            if (str[i].toInt() < 0x80) { continue }
+            code = getByteCode(str.substring(i, i + 1))
+            data = read(code[0], code[1])
+            if(data == null) { break }
+
+            byteCount = 0
+            var lineCount = 0
+            for (line in 0 until all_16_32) {
+
+                for (k in 0 until all_2_4) {
+
+//                    var binaryString = ""
+                    for (j in 0..7) {
+
+                        if (data[byteCount].toInt() shr (7 - j) and 0x1 == 1) {
+//                            print("❄")
+//                            binaryString += "1"
+                        } else {
+//                            print(" ")
+//                            binaryString += "0"
+                        }
+
+                    }
+                    byteCount++
+
+//                    val decimalValue = Integer.valueOf(binaryString, 2)
+//                    val hexValue = Integer.toHexString(decimalValue)
+//                    val hexValue = "%02X".format(decimalValue)
+
+//                    val byteArray = byteArrayOf('0'.toByte(), 'x'.toByte(), hexValue[0].toUpperCase().toByte(), hexValue[1].toUpperCase().toByte())
+
+//                    list.add(byteArray)
+//                    println("$lineCount: $hexValue")
+                    lineCount++
+                }
+//                println()
+            }
+        }
+
+        var length = 1
+        if(data != null) {
+
+            length = data.size.plus(length)
+        }
+
+        val headers = byteArrayOf(0xFF.toByte(), 0xEE.toByte(), 0x01.toByte(), 0x00.toByte(), str.length.toByte())
+
+        if(data == null) {
+
+            return headers
+        }
+
+        return headers.plus(data)
+    }
+
+    private fun read(areaCode: Int, posCode: Int): ByteArray? {
+
+        var data: ByteArray? = null
+        val assManager = context.assets
+
+        try {
+            val area = areaCode - 0xa0
+            val pos = posCode - 0xa0
+            val ins: InputStream = assManager.open("fonts/hzk16y")//FileInputStream(File(fontPath)) //Files.newInputStream(Paths.get(fontPath), StandardOpenOption.READ)
+
+            val offset = (all_32_128 * ((area - 1) * 94 + pos - 1)).toLong()
+            ins.skip(offset)
+            data = ByteArray(all_32_128)
+            ins.read(data, 0, all_32_128)
+            ins.close()
+
+        } catch (ex: Exception) {
+
+            System.err.println("SORRY,THE FILE CAN'T BE READ")
+        }
+
+        return data
+    }
+
+    private fun getByteCode(str: String): IntArray {
+
+        val byteCode = IntArray(2)
+        try {
+
+            val data = str.toByteArray(charset("GB2312"))
+            byteCode[0] = if (data[0] < 0) 256 + data[0] else 0 + data[0]
+            byteCode[1] = if (data[1] < 0) 256 + data[1] else 0 + data[1]
+
+        } catch (ex: Exception) {
+
+            ex.printStackTrace()
+        }
+
+        return byteCode
+    }
+
+}
--- app/src/test/java/com/ibm/cio/study/study/application/ExampleUnitTest.kt
+++ app/src/test/java/com/ibm/cio/study/study/application/ExampleUnitTest.kt
@@ -1,17 +1,50 @@
 package com.ibm.cio.study.study.application
 
+import com.ibm.cic.kotlin.starterkit.application.MainActivity
+import com.ibm.cic.kotlin.starterkit.helpers.MatrixHelper
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import kotlin.experimental.and
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+
+//        val str = "一二三四五六七八九十一二三四五     ";
+//        println(str.length)
+//        assertEquals(4, 2 + 2)
+        val charset = Charsets.UTF_8
+        val byteArray = 0x91
+        println(byteArray)
+    }
+
+    @Test
+    fun matrixHelper() {
+
+        val activity = Robolectric.setupActivity(MainActivity::class.java)
+        val matrixHelper = MatrixHelper(activity)
+        val list = matrixHelper.getMatrixList("测试字符串", 0)
+
+        val byteList: ArrayList<Byte> = ArrayList(list.size* 4)
+
+        list.forEach {
+
+
+        }
+
+        println(list.size)
+
+//        val decimalValue = Integer.valueOf(binaryString, 2)
+//        println(Integer.toHexString(decimalValue));
     }
 }
