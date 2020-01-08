cf2bc0a59b714c5e4c09b93a2b8894f4f2cf7ad3
Michael <mihui@cn.ibm.com>
2020-01-08 08:00:47 +0800

    Updated the packets

--- app/src/main/kotlin/com/ibm/cic/kotlin/starterkit/helpers/MatrixHelper.kt
+++ app/src/main/kotlin/com/ibm/cic/kotlin/starterkit/helpers/MatrixHelper.kt
@@ -7,117 +7,139 @@ import kotlin.collections.ArrayList
 
 class MatrixHelper(private val context: Context) {
 
-private val all_16_32 = 16
-private val all_2_4 = 2
-private val all_32_128 = 32
+    private val all_16_32 = 16
+    private val all_2_4 = 2
+    private val all_32_128 = 32
 
-private fun toByteArray(x: Char, y: Char): ByteArray {
+    /**
+     * Hex string to Byte Array
+     * @param hexString the hex string
+     * *
+     * @return ByteArray
+     */
+    private fun hexStringToBytes(v: String): ByteArray {
 
-return byteArrayOf('0'.toByte(), 'x'.toByte(), x.toUpperCase().toByte(), y.toUpperCase().toByte())
-}
+        var hexString = v
+        hexString = hexString.toUpperCase()
+        val length = hexString.length / 2
+        val hexChars = hexString.toCharArray()
+        val d = ByteArray(length)
+        for (i in 0 until length) {
+            val pos = i * 2
+            d[i] = (charToByte(hexChars[pos]).toInt() shl 4 or charToByte(hexChars[pos + 1]).toInt()).toByte()
+        }
+        return d
+    }
+    /**
+     * Convert char to byte
+     * @param c char
+     * *
+     * @return byte
+     */
+    private fun charToByte(c: Char): Byte {
 
-fun getMatrixList(str: String, index: Int): ByteArray {
+        return "0123456789ABCDEF".indexOf(c).toByte()
+    }
 
-var data: ByteArray? = null
-var code: IntArray? = null
-var byteCount: Int
+    private fun toByteArray(x: Char, y: Char): ByteArray {
 
-for (i in str.indices) {
+        return byteArrayOf('0'.toByte(), 'x'.toByte(), x.toUpperCase().toByte(), y.toUpperCase().toByte())
+    }
 
-if (str[i].toInt() < 0x80) { continue }
-code = getByteCode(str.substring(i, i + 1))
-data = read(code[0], code[1])
-if(data == null) { break }
+    fun getMatrixList(str: String, index: Int): ByteArray {
 
-byteCount = 0
-var lineCount = 0
-for (line in 0 until all_16_32) {
+        var data: ByteArray? = null
+        var code: IntArray? = null
+        var byteCount: Int
+        var list: ByteArray
+        var headers = byteArrayOf(0xFF.toByte(), 0xEE.toByte(), 0x01.toByte(), 0x00.toByte(), str.length.toByte())
 
-for (k in 0 until all_2_4) {
+        for (i in str.indices) {
 
-//                    var binaryString = ""
-for (j in 0..7) {
+            if (str[i].toInt() < 0x80) {
+                continue
+            }
+            code = getByteCode(str.substring(i, i + 1))
+            data = read(code[0], code[1])
+            if (data == null) { break }
+            byteCount = 0
+            var lineCount = 0
 
-if (data[byteCount].toInt() shr (7 - j) and 0x1 == 1) {
+            for (line in 0 until all_16_32) {
+                for (k in 0 until all_2_4) {
+                    var binaryString = ""
+                    for (j in 0..7) {
+
+                        if (data[byteCount].toInt() shr (7 - j) and 0x1 == 1) {
 //                            print("❄")
-//                            binaryString += "1"
-} else {
+                            binaryString += "1"
+                        }
+                        else {
 //                            print(" ")
-//                            binaryString += "0"
-}
+                            binaryString += "0"
+                        }
+                    }
+                    byteCount++
 
-}
-byteCount++
-
-//                    val decimalValue = Integer.valueOf(binaryString, 2)
+                    val decimalValue = Integer.valueOf(binaryString, 2)
 //                    val hexValue = Integer.toHexString(decimalValue)
-//                    val hexValue = "%02X".format(decimalValue)
+                    val hexValue = "%02X".format(decimalValue)
 
+                    val byte = hexStringToBytes(hexValue)
+                    println(hexValue+": "+byte.size)
+                    headers += (byte)
 //                    val byteArray = byteArrayOf('0'.toByte(), 'x'.toByte(), hexValue[0].toUpperCase().toByte(), hexValue[1].toUpperCase().toByte())
 
-//                    list.add(byteArray)
+                    //list.add(byteArray)
 //                    println("$lineCount: $hexValue")
-lineCount++
-}
+                    lineCount++
+                }
 //                println()
-}
-}
+            }
+        }
 
-var length = 1
-if(data != null) {
+        return headers
+    }
 
-length = data.size.plus(length)
-}
+    private fun read(areaCode: Int, posCode: Int): ByteArray? {
 
-val headers = byteArrayOf(0xFF.toByte(), 0xEE.toByte(), 0x01.toByte(), 0x00.toByte(), str.length.toByte())
+        var data: ByteArray? = null
+        val assManager = context.assets
 
-if(data == null) {
+        try {
+            val area = areaCode - 0xa0
+            val pos = posCode - 0xa0
+            val ins: InputStream = assManager.open("fonts/hzk16y")//FileInputStream(File(fontPath)) //Files.newInputStream(Paths.get(fontPath), StandardOpenOption.READ)
 
-return headers
-}
+            val offset = (all_32_128 * ((area - 1) * 94 + pos - 1)).toLong()
+            ins.skip(offset)
+            data = ByteArray(all_32_128)
+            ins.read(data, 0, all_32_128)
+            ins.close()
 
-return headers.plus(data)
-}
+        } catch (ex: Exception) {
 
-private fun read(areaCode: Int, posCode: Int): ByteArray? {
+            System.err.println("SORRY,THE FILE CAN'T BE READ")
+        }
 
-var data: ByteArray? = null
-val assManager = context.assets
+        return data
+    }
 
-try {
-val area = areaCode - 0xa0
-val pos = posCode - 0xa0
-val ins: InputStream = assManager.open("fonts/hzk16y")//FileInputStream(File(fontPath)) //Files.newInputStream(Paths.get(fontPath), StandardOpenOption.READ)
+    private fun getByteCode(str: String): IntArray {
 
-val offset = (all_32_128 * ((area - 1) * 94 + pos - 1)).toLong()
-ins.skip(offset)
-data = ByteArray(all_32_128)
-ins.read(data, 0, all_32_128)
-ins.close()
+        val byteCode = IntArray(2)
+        try {
 
-} catch (ex: Exception) {
+            val data = str.toByteArray(charset("GB2312"))
+            byteCode[0] = if (data[0] < 0) 256 + data[0] else 0 + data[0]
+            byteCode[1] = if (data[1] < 0) 256 + data[1] else 0 + data[1]
 
-System.err.println("SORRY,THE FILE CAN'T BE READ")
-}
+        } catch (ex: Exception) {
 
-return data
-}
+            ex.printStackTrace()
+        }
 
-private fun getByteCode(str: String): IntArray {
-
-val byteCode = IntArray(2)
-try {
-
-val data = str.toByteArray(charset("GB2312"))
-byteCode[0] = if (data[0] < 0) 256 + data[0] else 0 + data[0]
-byteCode[1] = if (data[1] < 0) 256 + data[1] else 0 + data[1]
-
-} catch (ex: Exception) {
-
-ex.printStackTrace()
-}
-
-return byteCode
-}
+        return byteCode
+    }
 
 }
--- app/src/test/java/com/ibm/cio/study/study/application/ExampleUnitTest.kt
+++ app/src/test/java/com/ibm/cio/study/study/application/ExampleUnitTest.kt
@@ -6,45 +6,65 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
-import kotlin.experimental.and
 
 /**
-* Example local unit test, which will execute on the development machine (host).
-*
-* See [testing documentation](http://d.android.com/tools/testing).
-*/
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
 @RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
 
-@Test
-fun addition_isCorrect() {
+    fun hexStringToBytes(hexString: String?): ByteArray? {
+        var hexString = hexString
+        if (hexString == null || hexString == "") {
+            return null
+        }
+        hexString = hexString.toUpperCase()
+        val length = hexString.length / 2
+        val hexChars = hexString.toCharArray()
+        val d = ByteArray(length)
+        for (i in 0..length - 1) {
+            val pos = i * 2
+            d[i] = (charToByte(hexChars[pos]).toInt() shl 4 or charToByte(hexChars[pos + 1]).toInt()).toByte()
+        }
+        return d
+    }
+
+    private fun charToByte(c: Char): Byte { return "0123456789ABCDEF".indexOf(c).toByte() }
+
+    @Test
+    fun addition_isCorrect() {
 
 
 //        val str = "一二三四五六七八九十一二三四五     ";
 //        println(str.length)
 //        assertEquals(4, 2 + 2)
-val charset = Charsets.UTF_8
-val byteArray = 0x91
-println(byteArray)
-}
+//        val charset = Charsets.UTF_8
+//        val byteArray = 0x91
+//        println(byteArray)
+        val binaryString = "1001";
+        val decimalValue = Integer.valueOf(binaryString, 2)
+        val hexValue = "%02X".format(decimalValue)
+        println(hexStringToBytes(hexValue)?.size)
+    }
 
-@Test
-fun matrixHelper() {
+    @Test
+    fun matrixHelper() {
 
-val activity = Robolectric.setupActivity(MainActivity::class.java)
-val matrixHelper = MatrixHelper(activity)
-val list = matrixHelper.getMatrixList("测试字符串", 0)
+        val activity = Robolectric.setupActivity(MainActivity::class.java)
+        val matrixHelper = MatrixHelper(activity)
+        val list = matrixHelper.getMatrixList("测试字符串", 0)
 
-val byteList: ArrayList<Byte> = ArrayList(list.size* 4)
+//        val byteList: ArrayList<Byte> = ArrayList(list.size * 4)
 
-list.forEach {
+        list.forEach {
 
+        }
 
-}
-
-println(list.size)
+        println(list.size)
 
 //        val decimalValue = Integer.valueOf(binaryString, 2)
 //        println(Integer.toHexString(decimalValue));
-}
+    }
 }
